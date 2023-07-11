9c04209a8983e1b2d427b81752e4827b280628d8
Limin Ma <limin.ma@ontariotechu.net>
2023-07-11 12:38:25 -0400

    refactor PDFChatBot.

--- src/pdf_chatbot.py
+++ src/pdf_chatbot.py
@@ -16,68 +16,70 @@ DEFAULT_EMBED_MODEL = 'sentence-transformers/all-mpnet-base-v2'
 DEFAULT_MODEL = 'facebook/bart-large-cnn'
 
 
+def load_llm():
+    """
+    load the summarization model using the HuggingFace pipeline,
+    use GPU if possible
+    """
+    return pipeline(task="summarization", model=DEFAULT_MODEL, device_map='auto')
+
+
+def load_emb():
+    """
+    load the embedding model,
+    use GPU if possible
+    """
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    return HuggingFaceEmbeddings(
+        model_name=DEFAULT_EMBED_MODEL,
+        model_kwargs={'device': device}
+    )
+    
+    
 class PDFChatBot:
-    def __init__(self,
-                 pdf_filepath: str,
-                 chunk_size: int = None,
-                 chunk_overlap: int = None,
-                 chunking_separators: list[str] = None
-                ):
-        if pdf_filepath is not None:
-            self.pdf_filepath = pdf_filepath
-        else:
-            raise ValueError("Param 'pdf_filepath' can not be None.")
-        
-        # do some validation for chunking configuration
-        self.embedding_configs = {
-            'chunk_size': chunk_size if chunk_size and chunk_size > 0 else 1500,
-            'chunk_overlap': chunk_overlap if chunk_overlap and chunk_overlap > 0 else 150,
-            'chunking_separators': chunking_separators or ["\n\n", "\n", "(?<=\. )", " ", ""]
-        }
-        
-        self.vectordb = None
+    def __init__(self) -> None:
         self.llm = None
+        self.emb_model = None
+        self.vectordb = None
         
-        self._init_vectordb()
-        self._load_llm()
 
+    def load_vectordb(self,
+                      pdf_filepath: str,
+                      chunk_size: int = None,
+                      chunk_overlap: int = None,
+                      chunking_separators: list[str] = None) -> None:
+        if pdf_filepath is None:
+            raise ValueError("Param 'pdf_filepath' can not be None.")
 
-    def _init_vectordb(self):
+        # do some validation for chunking configurations
+        chunk_size = chunk_size if chunk_size and chunk_size > 0 else 1500
+        chunk_overlap = chunk_overlap if chunk_overlap and chunk_overlap > 0 else 150
+        chunking_separators = chunking_separators or ["\n\n", "\n", "(?<=\. )", " ", ""]
+        
         # load the pdf file first
-        self._pdf_pages = PyPDFLoader(self.pdf_filepath).load()
+        self._pdf_pages = PyPDFLoader(pdf_filepath).load()
 
         # use RecursiveCharacterTextSplitter for splitting generic text
         text_splitter = RecursiveCharacterTextSplitter(
-            chunk_size=self.embedding_configs['chunk_size'],
-            chunk_overlap=self.embedding_configs['chunk_overlap'],
-            separators=self.embedding_configs['chunking_separators']
+            chunk_size=chunk_size,
+            chunk_overlap=chunk_overlap,
+            separators=chunking_separators
         )
         self._docs = text_splitter.split_documents(self._pdf_pages)
-        
-        # load the embedding model
-        device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        embedding = HuggingFaceEmbeddings(
-            model_name=DEFAULT_EMBED_MODEL,
-            model_kwargs={'device': device}
-        )
         
         # use Chroma as vector db.
         # no need to persist the database
         self.vectordb = Chroma.from_documents(
             documents=self._docs,
-            embedding=embedding,
+            embedding=self.emb_model,
             persist_directory=None
         )
     
     
-    def _load_llm(self):
-        self.llm = pipeline(task="summarization", model=DEFAULT_MODEL, device_map='auto')
-    
-    
     def mmr_search(self, query, k=5, fetch_k=7, max_length=130, min_length=30):
         """This method uses maximal marginal relevance (mmr) to search the vector database, 
-        then summarizes each chunk, and finally summarizes the summaries of all chunks
-        from the previous step.
+        then summarizes each chunk, and finally summarizes the summaries of all chunks from
+        the previous step.
         """
         # retrieve relevant chunks from the vector database
         src_docs = self.vectordb.max_marginal_relevance_search(query, k=k, fetch_k=fetch_k)
